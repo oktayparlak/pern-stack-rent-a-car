3fd82e90a685e79aac923fcda1e6dc4b7bcc33d8
Oktay <oktay.parlak.12@hotmail.com>
2025-01-20 16:48:04 +0300

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box p={4}>
+      <Heading mb={4}>Araç Kiralama</Heading>
+      <Text>Hoş geldiniz! Bu bir araç kiralama uygulamasıdır.</Text>
+    </Box>
+  );
+}
+
+export default App;
